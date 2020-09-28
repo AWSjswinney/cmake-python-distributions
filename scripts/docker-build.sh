4d3b0c6528b62c0dd973fff7b21802dd0dd6f2be
Jonathan Swinney <jswinney@amazon.com>
2020-09-28 16:28:11 +0000

    native build scripts

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
 MANYLINUX_PYTHON=cp38-cp38
+export PATH="/opt/python/${MANYLINUX_PYTHON}/bin:$PATH"
+export DEFAULT_DOCKCROSS_IMAGE=dockcross/manylinux2014-aarch64
+
+
+yum install -y openssl-devel zlib-devel libcurl-devel
+CMAKE_VERSION=3.12.0
+cd /root
+curl -L -O https://cmake.org/files/v3.12/cmake-${CMAKE_VERSION}.tar.gz
+tar -xzf cmake-${CMAKE_VERSION}.tar.gz
+cd cmake-${CMAKE_VERSION}
+./bootstrap --system-curl
+make -j$(nproc)
+make install
+cd ..
 
 cd /io
 
+
 ci_before_install() {
     /opt/python/${MANYLINUX_PYTHON}/bin/python scripts/ssl-check.py
-    /opt/python/${MANYLINUX_PYTHON}/bin/python -m pip install --disable-pip-version-check --upgrade pip
-    /opt/python/${MANYLINUX_PYTHON}/bin/pip install -U scikit-ci scikit-ci-addons
-    /opt/python/${MANYLINUX_PYTHON}/bin/ci_addons --install ../addons
+    /opt/python/${MANYLINUX_PYTHON}/bin/pip install scikit-ci scikit-ci-addons scikit-build
 }
 
 ci_install() {
